afa955b96c78d005bf76a5b239c87a9ae2d56d34
clintbonnett-acg <80351457+clintbonnett-acg@users.noreply.github.com>
2025-08-22 09:14:08 -0400

    Add files via upload

---
+++ vmprep.ps1
@@ -0,0 +1,8 @@
+Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideClock" -Value 1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "DisableNotificationCenter" -Value 1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "HideSCAVolume" -Value 1
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install microsoftazurestorageexplorer -y --ignore-checksums
